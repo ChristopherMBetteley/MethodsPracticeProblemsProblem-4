eba7b4e8d9f6c5de22b66a4db1f517e86701ad2b
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-14 17:56:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//calculate the sum of the elements in an array
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPracticeProblemsProblem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] example = { 20, 10, 5, 2 };
+            Console.WriteLine("The sum of the example array is {0}", Sum(example));
+
+        }
+
+        static int Sum(int[] numbers)//calculates and returns the sum of an array
+        {
+            int totalSum = 0;
+
+            for(int i = 0; i<numbers.Length; i++)
+            {
+                totalSum = totalSum + numbers[i];
+            }
+
+            return totalSum;
+        }
+    }
+}
